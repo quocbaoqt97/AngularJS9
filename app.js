0de941ae5a454781e6eccdb8d77775a55212ca45
Alex <Alexnguyen@coursera.com>
2019-12-05 22:12:05 +0700

    webapp

---
+++ app.js
@@ -0,0 +1,41 @@
+(function(){
+  'use strict';
+  angular.module('webapp',[])
+  .controller('ShoppingListController',ShoppingListController)
+  .controller('ShoppingListShow',ShoppingListShow)
+  .provider('ShoppingListService',ShoppingListService);
+  ShoppingListController.$inject=['ShoppingListService'];
+  function ShoppingListController(ShoppingListService){
+    var list = this;
+    list.itemName = "";
+    list.itemQuantity = "";
+    list.additem = function(){
+      try{
+        ShoppingListService.addItem(list.itemName,list.itemQuantity);
+      }
+      catch(error){
+        list.errorMessage = error.Message;
+      }
+    };
+  }
+  ShoppingListShow.$inject=['ShoppingListService'];
+  function ShoppingListShow('ShoppingListService'){
+    var showlist = this;
+    showlist.items = ShoppingListService.getItems();
+  }
+  function ShoppingListService(){
+    var service = this;
+    var items=[];
+    service.addItem = function(itemname,itemquantity){
+    var item = {
+      name:itemname,
+      quantity:itemquantity
+    };
+    items.push(item);
+    };
+    service.getItems = function(){
+      return items;
+    };
+  }
+
+})();
